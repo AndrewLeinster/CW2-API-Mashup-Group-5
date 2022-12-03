625fb737843b9fdc60535dd2f370573f9a1761d3
2419746 <2419746@dundee.ac.uk>
2022-12-03 00:11:51 +0000

    used different weather api to get date time info

--- Javascript/weather.js
+++ Javascript/weather.js
@@ -1,46 +1,44 @@
-// create helper functions
-function createNode(element)
-{
+
+function createNode(element) {
     return document.createElement(element);
 }
 
-function append(parent, el)
-{
+function append(parent, el) {
     return parent.appendChild(el);
 }
 
-document.getElementById("submit").addEventListener("click", function(){getData()});
+document.getElementById("submit").addEventListener("click", function () { getData() });
 function getData() {
     const weatherContainer = document.getElementById("weather");
     let searchedCity = document.getElementById("location").value;
-    // let searchedTime = document.getElementById("dateTime").value;
-    // this gets next forecast
-    // you can only get forecasts 3 hourly, so 12, 3, 6, so unusre how this works
-    url = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchedCity + "&appid=6fdb5d56148a837763b84d0f303bf297";
+    url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + searchedCity + "/2022-12-03/?key=5KP9KEBYVW933PV52J78QTRGX";
     console.log(url)
     console.log(searchedCity)
     fetch(url)
         // convert to JSON
         .then((resp) => resp.json())
-        .then(function(data) 
-        {
+        .then(function (data) {
             console.log(data);
-            var list = data.list;
-            console.log(list)
-            return list.map(function(list){
-                var weather = list.weather;
-                return weather.map(function(weath){
-                    var h1 = createNode("h1");
-                    var p = createNode("p");
-                    h1.innerHTML = "Weather: " + weath.main;
-                    p.innerHTML = "Description: " + weath.description;
-                    append(weatherContainer, h1);
-                    append(weatherContainer, p);
-                })
-                
+            var weather = data.days;
+            console.log(weather)
+            var h1 = createNode("h1");
+            h1.innerHTML = "City: " + data.address;
+            return weather.map(function (wether) {
+                var p = createNode("p");
+                var p1 = createNode("p");
+                var p2 = createNode("p");
+                p.innerHTML = "Conditions: " + wether.conditions;
+                p1.innerHTML = "Description: " + wether.description;
+                p2.innerHTML = "Date Time: " + wether.datetime;
+
+                append(weatherContainer, h1);
+                append(weatherContainer, p);
+                append(weatherContainer, p2);
+                append(weatherContainer, p1);
             })
+
         })
-        .catch(function(error) {
+        .catch(function (error) {
             console.log(error);
-          });
+        });
 }
